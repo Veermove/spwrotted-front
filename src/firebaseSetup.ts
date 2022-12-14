71c14f91b7f721b3c3a468b3414588a9871ba687
matsusel <mati.susel@wp.pl>
2022-12-14 01:08:18 +0100

    Firebase
    Implementacja Firebase potrzeba dać właściwe dane do firebaseSetup

---
+++ src/firebaseSetup.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfaWPf-67RgCEmF_tdqrjJZHEhzPzOI9U",
+  authDomain: "fir-auth-tut-622be.firebaseapp.com",
+  projectId: "fir-auth-tut-622be",
+  storageBucket: "fir-auth-tut-622be.appspot.com",
+  messagingSenderId: "548365078338",
+  appId: "1:548365078338:web:0dffb8e16a479c082a02db",
+}; //this is where your firebase app values you copied will go
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
